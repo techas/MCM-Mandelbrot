5b14d63c2792721ed2a5dd2575f04dee374efc8e
Sergio Zlotnik <techas@gmail.com>
2012-10-23 16:44:22 -0700

    Add first empty file

---
+++ main.m
@@ -0,0 +1,8 @@
+clear all
+clc
+%
+% Erasmus Mundus Master of Science in Computational Mechanics
+% Programming for Engineers and Scientists
+%
+% Matlab parallel implementation of a zoom in the Mandelbrot Set
+%
