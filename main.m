1b9e5f6897b250d1f00dcc66df404bca633363e3
Sergio Zlotnik <techas@gmail.com>
2012-11-07 05:30:48 -0800

    testing svn

--- main.m
+++ main.m
@@ -4,5 +4,5 @@ clc
 % Erasmus Mundus Master of Science in Computational Mechanics
 % Programming for Engineers and Scientists
 %
-% Matlab parallel implementation of a zoom in the Mandelbrot Set
+% Matlab parallel implementation of a zoom in the Mandelbrot Set.
 %
